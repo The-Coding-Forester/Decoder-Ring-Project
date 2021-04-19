8b9aecf146dfb1f66fbdbd7d9f8ddfd1bb2f99ef
Stephanie Moore <smoore_forestry@outlook.com>
2021-04-19 11:45:51 -0400

    Possible solution to createShiftedArray helper function

--- src/caesar.js
+++ src/caesar.js
@@ -8,11 +8,29 @@ const caesarModule = (function () {
   //function needs to know alphabet, use array for indexing
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   //user inputted message needs to be turned into array for index shifting
-  function getArrayFromString(input) {
-    return Array.from(input);
+  function getArrayFromString(string) {
+    return Array.from(string);
   }
   //spaces in user message need to remain equal to " " and not be subject to shift
-  //
+  function createShiftedArray(string, shift) {
+    const shiftedArray = [];
+    //if index of array is equal to a letter, then shift that letter and push to new array. Else, return original index.
+    string.forEach((character) => {
+      if (alphabet.find((letter) => letter !== character)) {
+        shiftedArray.push(character);
+      } else {
+        alphabet.find((letter) => letter === character){
+          let newIndex;
+          if ((alphabet.indexOf + shift) <= 0) {   //should accept results of 0 and negatives then add to 26 for new index number
+            newIndex = (alphabet.indexOf + shift) + 26;
+          } else {
+            newIndex = (alphabet.indexOf + shift) % 26;
+          }
+          shiftedArray.push(alphabet[newIndex]);
+        }
+      }
+    })
+  }
   //user inputted message needs to be returned to a string
 
 
@@ -21,7 +39,7 @@ const caesarModule = (function () {
     if (shift === 0) return false;
     if (shift > 25) return false; // alphabet is 26 (25 because of 0 index) characters, you cannot shift beyond that in either direction
     if (shift < -25) return false;
-
+    const string = input.toLowerCase;
   }
 
   return {
