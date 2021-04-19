2ad2fd8bb82a5a8a2fb01db99a54e14ea5a663c6
Stephanie Moore <smoore_forestry@outlook.com>
2021-04-19 16:58:34 -0400

    Solution for caesar shift function, both encoding and decoding

--- src/caesar.js
+++ src/caesar.js
@@ -7,41 +7,49 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
   //function needs to know alphabet, use array for indexing
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
   //user inputted message needs to be turned into array for index shifting
   function getArrayFromString(string) {
     return Array.from(string);
   }
-  //spaces in user message need to remain equal to " " and not be subject to shift
-  function createShiftedArray(string, shift) {
-    const shiftedArray = [];
-    //if index of array is equal to a letter, then shift that letter and push to new array. Else, return original index.
-    string.forEach((character) => {
-      if (alphabet.find((letter) => letter !== character)) {
-        shiftedArray.push(character);
-      } else {
-        alphabet.find((letter) => letter === character){
-          let newIndex;
-          if ((alphabet.indexOf + shift) <= 0) {   //should accept results of 0 and negatives then add to 26 for new index number
-            newIndex = (alphabet.indexOf + shift) + 26;
-          } else {
-            newIndex = (alphabet.indexOf + shift) % 26;
-          }
-          shiftedArray.push(alphabet[newIndex]);
-        }
-      }
-    })
-  }
-  //user inputted message needs to be returned to a string
 
+  function shiftLetter(resultingIndex, shift) {
+    let newIndex = (resultingIndex + shift) % 26; //should accept results of 0 and negatives then add to 26 for new index number
+    if (newIndex < 0) {
+      newIndex = newIndex + 26
+    }
+    return alphabet[newIndex];
+  }
+
+  function createShiftedArray(string, shift) {
+    let shiftedArray = getArrayFromString(string).map((character) => {
+      //if index of array is equal to a letter, then shift that letter and push to new array. Else, return original index.
+      //spaces in user message need to remain equal to " " and not be subject to shift
+      if (alphabet.indexOf(character) === -1) {
+        return character
+      }
+      return shiftLetter(alphabet.indexOf(character), shift);
+    });
+    return shiftedArray;
+  }
 
   function caesar(input, shift, encode = true) {
     // your solution code here
-    if (shift === 0) return false;
-    if (shift > 25) return false; // alphabet is 26 (25 because of 0 index) characters, you cannot shift beyond that in either direction
-    if (shift < -25) return false;
-    const string = input.toLowerCase;
-  }
+    if (shift === 0 || shift > 25 || shift < -25) {
+      // alphabet is 26 characters (0 through 25), you cannot shift beyond that in either direction
+      return false;
+    }
 
+    const string = input.toLowerCase();
+    let updatedShift = shift;
+
+    if (encode === false) {
+      updatedShift = shift * -1;
+    }
+
+    //user inputted message needs to be returned to a string
+    return createShiftedArray(string, updatedShift).join("");
+  }
   return {
     caesar,
   };
