8648e810691795bce599195cc427cfa9e3daafe5
Stephanie Moore <smoore_forestry@outlook.com>
2021-04-20 23:35:23 -0400

    All encoding tests passing

--- src/polybius.js
+++ src/polybius.js
@@ -5,9 +5,55 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
+  const polybiusSquare = {
+    1: { 1: "a", 2: "b", 3: "c", 4: "d", 5: "e" },
+    2: { 1: "f", 2: "g", 3: "h", 4: "(i/j)", 5: "k" },
+    3: { 1: "l", 2: "m", 3: "n", 4: "o", 5: "p" },
+    4: { 1: "q", 2: "r", 3: "s", 4: "t", 5: "u" },
+    5: { 1: "v", 2: "w", 3: "x", 4: "y", 5: "z" },
+  }
+
+  const column = Object.keys(polybiusSquare);
+  let rowKey;
+
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+
+  function getRowKey(object, value) {
+    rowKey = getKeyByValue(object, value);
+    return rowKey
+  }
+
+  function getArrayFromString(string) {
+    return Array.from(string);
+  }
 
   function polybius(input, encode = true) {
     // your solution code here
+    //take input and make it lower case and make it into an array
+    const startingArray = getArrayFromString(input.toLowerCase());
+    const encodedArray = [];
+    //iterate through the startingArray
+    startingArray.forEach(character => {
+      if (character === " ") {
+        encodedArray.push(" ");
+      }
+      else if (character === "i" || character === "j") {
+        encodedArray.push("42")
+      }
+      else {
+        column.forEach((key) => {
+          if (getRowKey(polybiusSquare[key], character)) {
+            encodedArray.push(rowKey + key)
+          }
+        })
+      }
+    });
+    const encodedString = encodedArray.join("")
+    if (encode) {
+      return encodedString;
+    }
   }
 
   return {
