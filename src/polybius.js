3bd6fceac270fd7b43246a675ef1f9ebb1072e8a
Stephanie Moore <smoore_forestry@outlook.com>
2021-04-25 00:20:38 -0400

    Refactored Polybius.js code to reduce complexity and increase readability

--- src/polybius.js
+++ src/polybius.js
@@ -5,24 +5,29 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
+
+  //refactoring polybiusSquare from and object of objects to an object with letters as keys and numbers as values
   const polybiusSquare = {
-    1: { 1: "a", 2: "b", 3: "c", 4: "d", 5: "e" },
-    2: { 1: "f", 2: "g", 3: "h", 4: "(i/j)", 5: "k" },
-    3: { 1: "l", 2: "m", 3: "n", 4: "o", 5: "p" },
-    4: { 1: "q", 2: "r", 3: "s", 4: "t", 5: "u" },
-    5: { 1: "v", 2: "w", 3: "x", 4: "y", 5: "z" },
-  }
+    "a": "11", "b": "21", "c": "31", "d": "41", "e": "51",
+    "f": "12", "g": "22", "h": "32", "i": "42", "j": "42", "k": "52",
+    "l": "13", "m": "23", "n": "33", "o": "43", "p": "53",
+    "q": "14", "r": "24", "s": "34", "t": "44", "u": "54",
+    "v": "15", "w": "25", "x": "35", "y": "45", "z": "55"
+  };
 
-  const column = Object.keys(polybiusSquare);
-  let rowKey;
+  //creating reverse of polybiusSquare to reduce coding complexity
+  const reversePolybiusSquare = {}
 
-  function getKeyByValue(object, value) {
-    return Object.keys(object).find(key => object[key] === value);
-  }
+  for (let letter in polybiusSquare) {
+    //numberPair created to prevent confusion within for in loop, specifically in the if statement
+    const numberPair = polybiusSquare[letter];
 
-  function getRowKey(object, value) {
-    rowKey = getKeyByValue(object, value);
-    return rowKey
+    if (reversePolybiusSquare[numberPair]) {
+      reversePolybiusSquare[numberPair] = `(${reversePolybiusSquare[numberPair]}/${letter})`
+    } else {
+      reversePolybiusSquare[numberPair] = letter;
+    }
+
   }
 
   function getArrayFromString(string) {
@@ -32,25 +37,16 @@ const polybiusModule = (function () {
   function encodeInput(input) {
     //take input and make it lower case and make it into an array
     const startingArray = getArrayFromString(input.toLowerCase());
-    const encodedArray = [];
     //iterate through the startingArray
-    startingArray.forEach(character => {
+    //return an array with with letter characters replaced by number values and spaces conserved
+    return startingArray.map(character => {
       if (character === " ") {
-        encodedArray.push(" ");
+        return character;
+      } else {
+        return polybiusSquare[character];
       }
-      else if (character === "i" || character === "j") {
-        encodedArray.push("42")
-      }
-      else {
-        column.forEach((key) => {
-          if (getRowKey(polybiusSquare[key], character)) {
-            encodedArray.push(rowKey + key)
-          }
-        })
-      }
-      return encodedArray;
-    });
-    return encodedArray.join("");
+      //convert array into a string
+    }).join("");
   }
 
   function decodeInput(input) {
@@ -62,10 +58,11 @@ const polybiusModule = (function () {
         decodedArray.push(index);
       }
       else {
-        let columnNum = index;
-        let rowNum = startingArray[i + 1];
-        i = i + 1
-        decodedArray.push(polybiusSquare[rowNum][columnNum])
+        let tens = index;
+        let ones = startingArray[i + 1];
+        const numberKey = tens + ones;
+        i = i + 1;
+        decodedArray.push(reversePolybiusSquare[numberKey]);
       }
     }
     return decodedArray.join("");
