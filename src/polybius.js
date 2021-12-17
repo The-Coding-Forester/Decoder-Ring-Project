701d8f005b23fbdd96f2f781560fa64564aa090e
Stephanie Moore <smoore_forestry@outlook.com>
2021-12-17 15:14:53 -0500

    comment edited

--- src/polybius.js
+++ src/polybius.js
@@ -1,10 +1,5 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
 
   //refactoring polybiusSquare from and object of objects to an object with letters as keys and numbers as values
   const polybiusSquare = {
